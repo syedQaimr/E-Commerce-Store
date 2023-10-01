6bc4fac572c187e28cae09987df5817066b9f53a
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-10-01 15:23:40 +0500

    Update Middlewares

--- Backend/middlewares/errorHandler.js
+++ Backend/middlewares/errorHandler.js
@@ -1,11 +1,23 @@
+const ErrorHandler = require("../utils/errorhandler");
 
 const errorHandler = (error , req , res , next) => {
 
     error.statusCode = error.statusCode || 500;
     error.message = error.message || "Internal Server Message";
+
+    if(error.name == 'CastError'){
+        const message = `Resource Not Found Invalid : ${error.path}`
+        err = new ErrorHandler(message,400)
+    }
+
+    console.log(error.code)
+    if(error.code === 11000){
+        const message = `Duplicate ${Object.keys(error.keyValue)} Entered`
+        error = new ErrorHandler(message , 400)
+    }
     
 
-    return res.status(error.statusCode).json({ success: false, error: error });
+    return res.status(error.statusCode).json({ success: false, message: error.message });
 
 }
 
