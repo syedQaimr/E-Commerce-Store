c2da50370a66e1fc74a6c2a767582d00f09d896f
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-10-01 15:29:57 +0500

    Create userSchema

---
+++ Backend/models/user.js
@@ -0,0 +1,79 @@
+const moongose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { hash } = require('../config/index')
+const { ACCESS_TOKEN_SECRET, } = require('../config/index')
+const { JWT_EXPIRE_TIME, } = require('../config/index')
+const jwt = require('jsonWebToken')
+const crypto = require('crypto')
+
+
+
+
+const { Schema } = moongose
+
+const userSchema = new Schema({
+    name: { type: String, required: [true, "Please enter name"] },
+    email: { type: String, required: [true, "Please enter email"], unique: true, validate: [validator.isEmail, "Your Format is inCorrect"] },
+    password: { type: String, required: [true, "Please enter password"], minLength: [8, "Password should be greater than 8 characters"], select: false },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }
+    ,
+    role: {
+        type: String,
+        default: "user"
+    },
+
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+
+},
+    { timestamps: true }
+)
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        next()
+    }
+
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+})
+
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({ id: this._id }, ACCESS_TOKEN_SECRET, { expiresIn: JWT_EXPIRE_TIME })
+}
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    console.log(enteredPassword, this.password , await bcrypt.compare(enteredPassword, this.password) )
+
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.methods.getResetpasswordToken =  function () {
+
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000
+
+    return resetToken
+   
+}
+
+module.exports = moongose.model('User', userSchema, 'Users')
