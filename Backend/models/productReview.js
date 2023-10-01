d479585d72ae349242dc571b7d61e64f6c19ba9a
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-10-01 15:26:47 +0500

    Update productReviewSchema

--- Backend/models/productReview.js
+++ Backend/models/productReview.js
@@ -4,17 +4,16 @@ const { Schema } = moongose
 
 const productReviewSchema = new Schema({
    
-   
     numOfReviews: {
         type: Number,
         default: 0
     },
     reviews: [
         {
+            user: { type: moongose.SchemaTypes.ObjectId, ref: 'User' },
             name: { type: String, required: true, },
             rating: { type: Number, default: 0 },
-            Comment: { type: String, required: true, },
-
+            comment: { type: String, required: true, },
         }
     ],
    
