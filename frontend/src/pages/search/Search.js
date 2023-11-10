cede343dd7e9df635b838d8c6b8595dca08bbe09
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-11 04:59:04 +0500

    'AddSearch'

---
+++ frontend/src/pages/search/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './Search.css'
+import { useNavigate } from 'react-router-dom';
+
+
+function Search() {
+    const navigate = useNavigate();
+    const [keyword , setKeyword] = useState('');
+    const searchSubmitHandler = (e) =>{
+        if(keyword.trim()){
+            navigate(`/products/${keyword}`) 
+        }
+        else{
+            navigate("/products")
+        }
+    }
+  return (
+   <>
+   <form className='searchBox' onSubmit={searchSubmitHandler}>
+    <input type='text' placeholder='Search a Product.......' onChange={(e)=> setKeyword(e.target.value)}/>
+    <input type='submit' value="search"/>
+   </form>
+   </>
+  )
+}
+
+export default Search
