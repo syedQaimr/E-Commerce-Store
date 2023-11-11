cacb52352f7070c82e318eb8796b66ff6473bc2f
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-12 04:55:55 +0500

    'AddUserMenu'

---
+++ frontend/src/components/userMenu/UserMenu.js
@@ -0,0 +1,100 @@
+import React, { Fragment, useState } from "react";
+import "./UserMenu.css";
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import SpeedDial from '@mui/material/SpeedDial';
+
+import Backdrop from "@mui/material/Backdrop";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { logOut } from "../../actions/userAction";
+import { useDispatch, useSelector } from "react-redux";
+
+const UserMenu = ({ user }) => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const Navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const options = [
+    { icon: <ListAltIcon />, name: "Orders", func: orders },
+    { icon: <PersonIcon />, name: "Profile", func: account },
+    {
+      icon: (
+        <ShoppingCartIcon
+          style={{ color: cartItems.length > 0 ? "black" : "unset" }}
+        />
+      ),
+      name: `Cart(${cartItems.length})`,
+      func: cart,
+    },
+    { icon: <ExitToAppIcon />, name: "Logout", func: logoutUser },
+  ];
+
+  if (user.role === "admin") {
+    options.unshift({
+      icon: <DashboardIcon />,
+      name: "Dashboard",
+      func: dashboard,
+    });
+  }
+
+  function dashboard() {
+    Navigate("/admin");
+  }
+
+  function orders() {
+    Navigate("/orders");
+  }
+  function account() {
+    Navigate("/account");
+  }
+  function cart() {
+    Navigate("/cart");
+  }
+  function logoutUser() {
+    dispatch(logOut());
+    alert.success("Logout Successfully");
+    Navigate('/');
+  }
+
+  return (
+    <Fragment>
+      <Backdrop open={open} style={{ zIndex: "10" }} />
+      <SpeedDial
+        ariaLabel="SpeedDial tooltip example"
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        style={{ zIndex: "11" }}
+        open={open}
+        direction="down"
+        className="speedDial"
+        icon={
+          <img
+            className="speedDialIcon"
+            src={user.avatar ? user.avatar: "/Profile.png"}
+            alt="Profile"
+          />
+        }
+      >
+        {options.map((item) => (
+          <SpeedDialAction
+            key={item.name}
+            icon={item.icon}
+            tooltipTitle={item.name}
+            onClick={item.func}
+            tooltipOpen={window.innerWidth <= 600 ? true : false}
+          />
+        ))}
+      </SpeedDial>
+    </Fragment>
+  );
+};
+
+export default UserMenu;
