23b67d44d548c340cb2faf6a77a7aeab71cabc52
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-12 23:47:23 +0500

    'CreateStore'

---
+++ frontend/src/store/store.js
@@ -0,0 +1,46 @@
+import { createStore , combineReducers , applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
+import { composeWithDevTools} from 'redux-devtools-extension'
+import {deleteProductReducer, editProductReducer, newProductReducer, newReviewReducer, productDetailsReducer, productReducer} from "../reducers/productReducer";
+import { userReducer , profileReducer  , resetPasswordReducer } from "../reducers/userReducer";
+import {cartReducer} from '../reducers/cartReducer'
+import {myOrdersReducer, newOrderReducer} from '../reducers/orderReducer'
+import { systemReducer } from "../reducers/systemReducer";
+
+// import { JSON } from "react-router-dom";
+
+
+
+const reducer = combineReducers({
+    products : productReducer,
+    prductDetails : productDetailsReducer,
+    user : userReducer,
+    profile:profileReducer,
+    forgotPassword : resetPasswordReducer,
+    cart : cartReducer,
+    newOrder : newOrderReducer,
+    myOrders : myOrdersReducer,
+    newReview : newReviewReducer,
+    systemSetting : systemReducer,
+    newProduct : newProductReducer,
+    deleteProduct : deleteProductReducer,
+    editProduct : editProductReducer,
+});
+
+let initialState ={
+    cart:{
+        cartItems:localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
+        shippingInfo : localStorage.getItem('shippingInfo')? JSON.parse(localStorage.getItem('shippingInfo')) : []
+    },
+
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default  store;
