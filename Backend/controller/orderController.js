c1cc70041057ab1c924ed6d20ed9b4a1c4b37bf7
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-21 23:18:30 +0500

    Update Order Controller

--- Backend/controller/orderController.js
+++ Backend/controller/orderController.js
@@ -3,6 +3,8 @@ const Order = require('../models/order');
 const PaymentInfo = require('../models/paymentInfo');
 const ShippingInfo = require('../models/ShippingInfo');
 const Product = require('../models/product');
+const moment = require('moment');
+
 
 
 const ApiFeatures = require('../utils/ApiFeatures');
@@ -24,7 +26,7 @@ const orderController = {
                 totalPrice,
             } = req.body;
 
-            console.log(req.body , req.user._id);
+            console.log(req.body, req.user._id);
 
 
             const newpaymentInfo = new PaymentInfo({ paymentInfo, paidAt: Date.now() });
@@ -74,7 +76,7 @@ const orderController = {
     async myOrder(req, res, next) {
 
         try {
-            const orders = await Order.find({ user: req.user._id }).populate(['user' , 'shippingInfo' , 'paymentInfo']);
+            const orders = await Order.find({ user: req.user._id }).populate(['user', 'shippingInfo', 'paymentInfo']);
 
             res.status(200).json({ success: true, orders, });
         }
@@ -102,7 +104,111 @@ const orderController = {
 
     },
 
+    async adminDashBoard(req, res, next) {
+        try {
+            // Total number of orders for the last seven days
+            const sevenDaysAgo = moment().subtract(7, 'days').startOf('day');
+            const totalOrdersLastSevenDays = await Order.countDocuments({
+                createdAt: { $gte: sevenDaysAgo.toDate() }
+            });
+    
+            // Total amount of all orders for the last seven days
+            const totalAmountLastSevenDays = await Order.aggregate([
+                {
+                    $match: {
+                        createdAt: { $gte: sevenDaysAgo.toDate() }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        total: { $sum: "$totalPrice" }
+                    }
+                },
+                {
+                    $sort: { _id: 1 }
+                }
+            ]);
+    
+            // Orders count date-wise for the last seven days
+            const ordersCountByDate = await Order.aggregate([
+                {
+                    $match: {
+                        createdAt: { $gte: sevenDaysAgo.toDate() }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $sort: { _id: 1 }
+                }
+            ]);
+    
+            // Products count date-wise for the last seven days
+            const productsCountByDate = await Product.aggregate([
+                {
+                    $match: {
+                        createdAt: { $gte: sevenDaysAgo.toDate() }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $sort: { _id: 1 }
+                }
+            ]);
+    
+            // Total number of orders
+            const totalOrders = await Order.countDocuments();
+    
+            // Total amount of all orders
+            const totalAmount = await Order.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        total: { $sum: "$totalPrice" }
+                    }
+                }
+            ]);
+    
+            // Total number of orders with orderStatus as 'Shippable'
+            const shippableOrders = await Order.countDocuments({ orderStatus: 'Shippable' });
+    
+            // Total number of products from Product Model
+            const totalProducts = await Product.countDocuments();
+    
+            // Four products with the highest rating
+            const topRatedProducts = await Product.find()
+                .sort({ ratings: -1 })
+                .limit(4);
+    
+            res.status(200).json({
+                success: true,
+                totalOrders,
+                totalAmount: totalAmountLastSevenDays,
+                totalIncom : totalAmount,
+                shippableOrders,
+                totalProducts,
+                topRatedProducts,
+                productsCountByDate,
+                ordersCountByDate,
+            });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    },
+
     async updateOrder(req, res, next) {
+        console.log(req.params.id, req.body.status)
+
 
         try {
             const order = await Order.findById(req.params.id);
@@ -115,19 +221,19 @@ const orderController = {
                 return next(new ErrorHandler("You have already delivered this order", 400));
             }
 
-            if (req.body.status === "Shipped") {
+            if (req.body.status === "Shipped" || req.body.status === "Processing") {
                 order.orderItems.forEach(async (o) => {
-                    console.log(o.product, o.quantity)
                     await updateStock(o.product, o.quantity);
                 });
             }
-            order.orderStatus = req.body.status;
+            order.orderStatus = "Delivered";
 
-            if (req.body.status === "Delivered") {
+
+            if (order.orderStatus === "Delivered") {
                 order.deliveredAt = Date.now();
             }
-
             await order.save({ validateBeforeSave: false });
+            console.log(order)
             res.status(200).json({ success: true, });
 
         }
@@ -154,7 +260,7 @@ const orderController = {
         }
 
     },
-   
+
 
 
 
@@ -164,11 +270,9 @@ const orderController = {
 async function updateStock(id, quantity) {
 
     const product = await Product.findById(id);
-    console.log(product)
 
     product.stock -= quantity;
 
-    console.log( product.stock , quantity)
 
     await product.save({ validateBeforeSave: false });
 }
