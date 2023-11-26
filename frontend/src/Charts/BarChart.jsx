be047ddf58fe33709f7961532f6a4fff53da848b
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-26 19:35:03 +0500

    Update  style in BarChart

--- frontend/src/Charts/BarChart.jsx
+++ frontend/src/Charts/BarChart.jsx
@@ -1,31 +1,58 @@
 import React from "react";
 import { Chart } from "react-google-charts";
 
-export const data = [
-  ["Year", "Sales", "Expenses", "Profit"],
-  ["2014", 1000, 400, 200],
-  ["2015", 1170, 460, 250],
-  ["2016", 660, 1120, 300],
-  ["2017", 1030, 540, 350],
-  ["2018", 1000, 400, 200],
-  ["2019", 1170, 460, 250],
-  ["2020", 660, 1120, 300],
-  ["2021", 1030, 540, 350],
-];
-
 export const options = {
   chart: {
     title: "Company Performance",
     subtitle: "Sales, Expenses, and Profit: 2014-2017",
   },
-  colors:["rgb(53,138,148)" , "rgb(37,11,165)" , "#188310"]
+  colors: ["rgb(53,138,148)", "rgb(37,11,165)", "#188310"],
+  vAxis: {
+    viewWindow: {
+      min: 0,
+      max: 100, // Set your desired maximum range for the y-axis
+    },
+    logScale: true, // Enable logarithmic scaling for the y-axis
+  },
+ 
 };
 
-export default function BarChart() {
+export default function BarChart(props) {
+  const data = [];
+
+  // Iterate through the last seven days
+  for (let i = 0; i < 7; i++) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() - i);
+    const formattedDate = currentDate.toISOString().split("T")[0];
+
+    const productsCount = props.data.productsCountByDate.find(
+      (item) => item._id === formattedDate
+    );
+    const ordersCount = props.data.ordersCountByDate.find(
+      (item) => item._id === formattedDate
+    );
+    const totalAmount = props.data.totalAmount.find(
+      (item) => item._id === formattedDate
+    );
+
+    data.push([
+      formattedDate,
+      totalAmount ? totalAmount.total : 0,
+      productsCount ? productsCount.count : 0,
+      ordersCount ? ordersCount.count : 0,
+    ]);
+  }
+
+  // Add header to the data
+  data.unshift(["Date", "Total Amount", "Products Count", "Orders Count"]);
+
+  console.log(data);
+
   return (
     <Chart
       chartType="Bar"
-      width="10s0%"
+      width="100%"
       height="350px"
       data={data}
       options={options}
