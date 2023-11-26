719c57a92a1bb621e3a96aadf6c321d7204afa3c
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-26 19:26:18 +0500

    Update path in ProtectedRoute

--- frontend/src/components/protectedRoute/ProtectedRoute.js
+++ frontend/src/components/protectedRoute/ProtectedRoute.js
@@ -2,15 +2,22 @@ import React from 'react'
 import {useSelector} from "react-redux"
 import {Navigate, useLocation} from "react-router-dom"
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({children , role=''}) => {
     const {isAuthenticated} = useSelector((state) => state.user);
+    const {user} = useSelector((state) => state.user);
+    console.log(user)
+
     let location = useLocation();
 
     if(!isAuthenticated) {
         console.log("hello --" , isAuthenticated)
-        return <Navigate to="/login" state={{ from: location}} replace />
+        return <Navigate to="/customer/login" state={{ from: location}} replace />
     }
     console.log("hello ++" , isAuthenticated)
+    if(role==='admin' && user.role !== 'admin'){
+        console.log("hello --" , isAuthenticated)
+        return <Navigate to="/" state={{ from: location}} replace />
+    }
  return children
 
 };
