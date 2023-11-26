e2c87fa75711733b24a340a6e97a005f721eec2a
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2023-11-26 19:31:52 +0500

    Update error response in userAction

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,7 @@
-import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, CLEAR_ERRORS , REGISTER_SUCCESS , REGISTER_FAIL , REGISTER_REQUEST , LOAD_USER_REQUEST , LOAD_USER_SUCCESS , LOAD_USER_FAIL , LOGOUT_FAIL , LOGOUT_SUCCESS , UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS ,  UPDATE_PASSWORD_FAIL , UPDATE_PASSWORD_REQUEST , UPDATE_PASSWORD_SUCCESS , FORGOT_PASSWORD_FAIL , FORGOT_PASSWORD_REQUEST , FORGOT_PASSWORD_SUCCESS, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS, RESET_PASSWORD_FAIL} from '../constants/userContant';
+import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, CLEAR_ERRORS , REGISTER_SUCCESS , REGISTER_FAIL , REGISTER_REQUEST , LOAD_USER_REQUEST , LOAD_USER_SUCCESS , LOAD_USER_FAIL , LOGOUT_FAIL , LOGOUT_SUCCESS , UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS ,  UPDATE_PASSWORD_FAIL , UPDATE_PASSWORD_REQUEST , UPDATE_PASSWORD_SUCCESS , FORGOT_PASSWORD_FAIL , FORGOT_PASSWORD_REQUEST , FORGOT_PASSWORD_SUCCESS, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS, RESET_PASSWORD_FAIL, GET_ALL_USER_REQUEST, GET_ALL_USER_SUCCESS, GET_ALL_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, DELETE_USER_FAIL} from '../constants/userContant';
 import {api} from '../apies/api';
+import ErrorPage from '../components/frontendErrorHandler/ErrorPage'
+
 
 
 export const login = (email , password)=> async (dispatch) =>{
@@ -8,7 +10,7 @@ export const login = (email , password)=> async (dispatch) =>{
 
 
         
-        const {data} = await api.post('/login' , {email , password});
+        const {data} = await api.post('/user/login' , {email , password});
 
         dispatch({ type : LOGIN_SUCCESS , payload: data.user})
     }
@@ -23,14 +25,20 @@ export const loadUser = ()=> async (dispatch) =>{
     try{
         dispatch({type : LOAD_USER_REQUEST});
 
-        const {data} = await api.get('/me');
+        const {data} = await api.get('/user/me');
+        console.log("Hello")
 
         dispatch({ type : LOAD_USER_SUCCESS , payload: data.user})
     }
     catch(error){
+        if(error.message === "Network Error"){
+            dispatch({type: LOAD_USER_FAIL , payload: error.message});
+        }
+        else{
         console.log("yes" , error.response)
         console.log(error.response.data.message);
         dispatch({type: LOAD_USER_FAIL , payload: error.response.data.message});
+        }
     }
 };
 
@@ -38,7 +46,7 @@ export const loadUser = ()=> async (dispatch) =>{
 export const logOut = ()=> async (dispatch) =>{
     try{
 
-        await api.get('/logout');
+        await api.get('/user/logout');
 
         dispatch({ type : LOGOUT_SUCCESS})
     }
@@ -54,7 +62,7 @@ export const register = (userData)=> async (dispatch) =>{
         dispatch({type : REGISTER_REQUEST});
 
 
-        const {data} = await api.post('/register' , userData );
+        const {data} = await api.post('/user/register' , userData );
        
 
         dispatch({type : REGISTER_SUCCESS , payload : data.user});
@@ -74,7 +82,7 @@ export const updateProfile = (user)=> async (dispatch) =>{
 
 
         
-        const {data} = await api.put('/me/update' , user);
+        const {data} = await api.put('/user/me/update' , user);
 
 
         dispatch({ type : UPDATE_PROFILE_SUCCESS , payload: data.success})
@@ -91,7 +99,7 @@ export const updatePassword = (passwords)=> async (dispatch) =>{
     try{
         dispatch({type : UPDATE_PASSWORD_REQUEST});
 
-        const {data} = await api.put('/password/update' , passwords);
+        const {data} = await api.put('/user/password/update' , passwords);
 
 
         dispatch({ type : UPDATE_PASSWORD_SUCCESS , payload: data.success})
@@ -106,7 +114,7 @@ export const forgotPassword = (email )=> async (dispatch) =>{
         dispatch({type : FORGOT_PASSWORD_REQUEST});
         console.log(email)
         
-        const {data} = await api.post('/password/forgot' , email);
+        const {data} = await api.post('/user/password/forgot' , email);
 
         dispatch({ type : FORGOT_PASSWORD_SUCCESS , payload: data.message})
     }
@@ -120,7 +128,7 @@ export const resetPassword = (resetPasswords )=> async (dispatch) =>{
     try{
         dispatch({type : RESET_PASSWORD_REQUEST});
         
-        const {data} = await api.put('/password/reset' , resetPasswords);
+        const {data} = await api.put('/user/password/reset' , resetPasswords);
 
         dispatch({ type : RESET_PASSWORD_SUCCESS , payload: data.message})
     }
@@ -129,6 +137,53 @@ export const resetPassword = (resetPasswords )=> async (dispatch) =>{
         dispatch({type: RESET_PASSWORD_FAIL , payload: error.response.data.message});
     }
 };
+
+
+
+export const getAllUsers = () => async (dispatch) => {
+
+    try {
+        dispatch({ type: GET_ALL_USER_REQUEST });
+
+        const { data } = await api.get('/admin/users');
+
+        dispatch({
+            type: GET_ALL_USER_SUCCESS,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: GET_ALL_USER_FAIL,
+            payload: error.response.data.error
+        })
+
+    }
+
+}
+
+
+export const deleteUser = (id) => async (dispatch) => {
+
+    console.log(id)
+    try {
+        dispatch({ type: DELETE_USER_REQUEST });
+        const { data } = await api.delete(`/admin/user/${id}`);
+
+        dispatch({
+            type: DELETE_USER_SUCCESS,
+            payload: data.success
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: DELETE_USER_FAIL,
+            payload: error.response.data.error
+        })
+
+    }
+
+}
 
 
 
